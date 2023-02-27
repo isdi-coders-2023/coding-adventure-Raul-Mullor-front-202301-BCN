4865451af153c680f7e2a057d8460f64e2a8de59
RaulMullor <mullor92@gmail.com>
2023-02-27 17:39:19 +0100

    Refactor Buttom component

--- src/components/TheButton/TheButtonStyled.tsx
+++ src/components/TheButton/TheButtonStyled.tsx
@@ -3,13 +3,22 @@ import "@fontsource/saira-stencil-one";
 
 const TheButtonStyled = styled.button`
   background-color: #cbcf18;
+  color: #1e1e1e;
   width: 323px;
   height: 40px;
   border-radius: 10px;
   border: none;
   font-family: "Saira Stencil One";
+  font-size: 20px;
 
+  :disabled {
+    opacity: 57%;
+  }
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    height: 57px;
+  }
 `;
 
 export default TheButtonStyled;
